5babe5f8864b0ecacdf488e8317663cd2999b50a
Flerys11 <118915509+Flerys11@users.noreply.github.com>
2023-07-04 22:35:08 +0200

    Add files via upload
    sprint7

---
+++ FrameW/TestFramework/Test/Emp.java
@@ -0,0 +1,41 @@
+package Test;
+
+import etu2044.framework.Url;
+import etu2044.framework.Modelview;
+
+public class Emp {
+    String nom;
+    Integer t;
+
+    public void setT(Integer t) {
+        this.t = t;
+    }
+    public Integer getT() {
+        return t;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getNom() {
+        return nom;
+    }
+    @Url(url="find-All")
+    public Modelview FindAll() {
+        Modelview m = new Modelview();
+        m.setView("Ay.jsp");
+        int u = 23;
+        m.addItem("data", u);
+        System.out.println(" find_all ");
+        return m;
+    }
+    
+    @Url(url="get-form")
+    public Modelview getForm() {
+        Modelview m = new Modelview();
+        m.setView("Ay.jsp");
+        System.out.println(this.getNom());
+        return m;
+    }
+
+}
