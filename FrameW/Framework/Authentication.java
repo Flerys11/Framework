b2c19d406104709ab5717adf108677574f6bb248
Flerys11 <118915509+Flerys11@users.noreply.github.com>
2023-07-04 22:45:32 +0200

    Add files via upload
    sprint13

---
+++ FrameW/Framework/Authentication.java
@@ -0,0 +1,9 @@
+package etu2044.framework;
+
+import java.lang.annotation.*;
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface Authentication {
+    String profile() default "";
+}
